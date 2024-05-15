cc2011d1808e59a34c5eb533267d1f058fa26cd9
daniyalshafiq1234 <86923049+daniyalshafiq1234@users.noreply.github.com>
2024-05-15 17:16:36 +0500

    index.jsx file updated

--- src/index.jsx
+++ src/index.jsx
@@ -1,4 +1,5 @@
-import { createRoot } from 'react-dom/client';
-const container = document.getElementById('app');
-const root = createRoot(container); 
-root.render(<App tab="home" />);
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+
+ReactDOM.render(<App />, document.getElementById("root"));
